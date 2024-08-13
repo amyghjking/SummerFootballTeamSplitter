459f13c9fa8a9797536ff28ad72f9f169bed93a0
amyghjking <amyking386@gmail.com>
2024-08-13 13:57:33 +1200

    Add name attributes to Player class

--- src/Player.java
+++ src/Player.java
@@ -5,6 +5,7 @@
  * - Their commitment to playing summer football
  */
 public class Player {
+  private String name;
   private String winterTeam;
   private String position;
   private String commitment;
@@ -12,15 +13,24 @@ public class Player {
   /**
    * Constructor initialises player attributes.
    *
+   * @param name Name of the player in firstname lastname format.
    * @param winterTeam Winter team played for in 2024.
    * @param position Position they play.
    * @param commitment Commitment to summer games.
    */
-  public Player(String winterTeam, String position, String commitment) {
+  public Player(String name, String winterTeam, String position, String commitment) {
+    this.name = name;
     this.winterTeam = winterTeam;
     this.position = position;
     this.commitment = commitment;
   }
+
+  /**
+   * Returns name.
+   *
+   * @return name.
+   */
+  public String getName() { return name; }
 
   /**
    * Returns winterTeam.
